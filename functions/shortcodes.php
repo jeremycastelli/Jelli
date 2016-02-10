2b97ea9b84aa414dc9092e9b9414f9f514107ce6
Castelli Jeremy <contact@jeremycastelli.com>
2016-02-10 22:39:53 +0100

    changed shortcodes

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -9,13 +9,9 @@ class jelli_shortcodes
 		// Shortcode_empty_paragraph_fix
 		add_filter('the_content', array(&$this,'shortcode_empty_paragraph_fix'));
 	
-		// Snapshot of a website using wordpress.com
-		add_shortcode( 'email', array(&$this,'email_encode'));
-
-		// Videos
-		add_shortcode("vimeo", array(&$this,'vimeo'));
-		add_shortcode("youtube", array(&$this,'youtube'));
-
+		
+		add_shortcode("col1", array(&$this,'col1'));
+		add_shortcode("col2", array(&$this,'col2'));
 	}
 	
 	
@@ -31,27 +27,14 @@ class jelli_shortcodes
 
 		return $content;
     } 
-
+    function col1( $atts, $content = null ) {
+        return '<div class="col1">'.$content.'</div>';
+    }
+    function col2( $atts, $content = null ) {
+        return '<div class="col2">'.$content.'</div>';
+    }
 	
-	function email_encode( $atts, $content ){
-		return '<a href="'.antispambot("mailto:".$content).'">'.antispambot($content).'</a>';
-	}
-	function vimeo( $atts, $content = null ) {
-		extract( shortcode_atts( array( 
-	    "id" => '', 
-	    "height" => '280',
-	    "width" => '500'
-	    ), $atts ) );
-	    return '<iframe src="http://player.vimeo.com/video/' . $id . '?title=0&amp;byline=0&amp;portrait=0" width="' . $width . '" height="' . $height . '" frameborder="0"></iframe>';
-	}
-	function youtube( $atts, $content = null ) {
-		extract( shortcode_atts( array( 
-	    "id" => '', 
-	    "height" => '280',
-	    "width" => '500'
-	    ), $atts ) );
-	    return '<iframe width="' . $width . '" height="' . $height . '" src="http://www.youtube.com/embed/' . $id . '?rel=0" frameborder="0" allowfullscreen></iframe>';
-	}
+	
 	
 }// end class
 ?>
